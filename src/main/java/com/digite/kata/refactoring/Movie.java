499aafc2150940751ca675a31786e13b5ea1638b
sbhatkande <sbhatkande@digite.com>
2021-06-02 22:41:02 +0530

    adding base files

---
+++ src/main/java/com/digite/kata/refactoring/Movie.java
@@ -0,0 +1,26 @@
+package com.digite.kata.refactoring;
+
+public class Movie {
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+    private String _title;
+    private int _priceCode;
+
+    public Movie(String title, int priceCode) {
+        _title = title;
+        _priceCode = priceCode;
+    }
+
+    public String getTitle() {
+        return _title;
+    }
+
+    public int getPriceCode() {
+        return _priceCode;
+    }
+
+    public void setPriceCode(int priceCode) {
+        _priceCode = priceCode;
+    }
+}
