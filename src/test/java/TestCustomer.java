1a28b4f02b65da4493941dfb8bece588f57151cf
Shraddha Jadhav <shraddha.jadhav@digite.com>
2021-06-03 22:59:28 +0530

    Interface segregation and dependency inversion

--- src/test/java/TestCustomer.java
+++ src/test/java/TestCustomer.java
@@ -18,6 +18,31 @@ class TestCustomer {
                 "You earned 1 frequent renter points");
     }
 
+    @Test
+    public  void testStatementForREGULARMovie()
+    {
+        Customer w_customer = new Customer("shraddha");
+        Rental w_rental1 = new Rental(new RegularMovie("Fast&Furious"), 7);
+        w_customer.addRental(w_rental1);
+        String w_receipt = w_customer.statement();
+        Assertions.assertEquals(w_receipt, "Rental Record for shraddha\n" +
+                "\tFast&Furious\t9.5\n" +
+                "Amount owed is 9.5\n" +
+                "You earned 1 frequent renter points");
+    }
+
+    @Test
+    public  void testStatementForNewMovie()
+    {
+        Customer w_customer = new Customer("shraddha");
+        Rental w_rental1 = new Rental(new NewMovie("Avengers"), 7);
+        w_customer.addRental(w_rental1);
+        String w_receipt = w_customer.statement();
+        Assertions.assertEquals(w_receipt, "Rental Record for shraddha\n" +
+                "\tAvengers\t21.0\n" +
+                "Amount owed is 21.0\n" +
+                "You earned 2 frequent renter points");
+    }
 
 
 }
